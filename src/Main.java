6e78e942dc37e32c55c88d63067cc50f1c8235c8
Scott Pantall <aspantall@gmail.com>
2017-05-02 21:07:37 -0600

    Uses HashSet to get rid of duplicates

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@
  */
 import java.io.*;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main (String[] args) throws IOException {
@@ -13,7 +18,16 @@ public class Main {
         
         while ((line = buffer.readLine()) != null) {
             line = line.trim();
-            System.out.println(line);
+            //System.out.println(line);
+            unique(line);
         }
+    }
+    
+    public static void unique(String line) {
+        String[] numbers = line.split(",");
+        Set<String> noDuplicates = new HashSet<String>(Arrays.asList(numbers));
+        
+        noDuplicates.forEach(number -> System.out.print(number + ","));
+        System.out.printf("%n");
     }
 }
